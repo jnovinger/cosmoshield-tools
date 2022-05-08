d5db9332e52afd33e29d6c6a8abc57d0724fff88
Jason Novinger <jnovinger@gmail.com>
2022-05-08 13:53:39 -0500

    Add umatrix.py, which generates importable umatrix rules

---
+++ cosmoshield_tools/cosmoshield.py
@@ -0,0 +1,19 @@
+import json
+
+from .interbloc_api_client.client import Client
+from .interbloc_api_client.api.cosmoshield import (
+    get_all_blacklisted_domains_cosmoshield_domains_blacklist_get as get_all_blacklisted_domains
+)
+
+
+BASE_URL = 'https://api.interbloc.org'
+
+client = Client(base_url=BASE_URL)
+
+
+def get_blacklisted_domains():
+    response = get_all_blacklisted_domains.sync_detailed(client=client)
+    if response.status_code != 200:
+        raise Exception('Invalid response')
+
+    return json.loads(response.content)
---
+++ cosmoshield_tools/umatrix.py
@@ -0,0 +1,67 @@
+import itertools
+from pathlib import Path
+
+import click
+
+from cosmoshield_tools.cosmoshield import get_blacklisted_domains
+
+
+ASSET_DIR = 'assets'
+OUTPUT_FILE = 'umatrix-rules.txt'
+OUTPUT_MIN_FILE = 'umatrix-rules.min.txt'
+
+
+def generate_lines_for_domain(entry, minimal: bool = False):
+    """Generates a comment/config/spacer line for each blacklisted domain
+
+    Entry is an entry as described by the Interbloc API. Expected fields are
+    domain, date, and reason. Domain is the only field required to generate
+    a valid entry.
+    """
+    domain = entry.get('domain', None)
+    if domain:
+
+        if not minimal:
+            date = entry.get('date', None)
+            reason = entry.get('reason', None)
+
+            comment_line = [f'# {domain}:']
+            if date:
+                comment_line.append(f'{date}')
+            if reason:
+                comment_line.append(f'{reason}')
+
+            yield ' '.join(comment_line)
+
+        yield f'* {domain} * block'
+
+        if not minimal:
+            yield ''
+
+
+@click.command
+@click.option(
+    '--minimal',
+    required=False,
+    default=False,
+    is_flag=True,
+    help=(
+        'Flag indicating whether non config lines for each domain should be '
+        'included in the output. Defaults to False.'
+    ),
+)
+def main(minimal: bool):
+    lines = []
+    blacklist = get_blacklisted_domains()
+    for entry in blacklist.values():
+        lines.extend(generate_lines_for_domain(entry, minimal))
+
+    output_path = Path(ASSET_DIR) / (
+        OUTPUT_MIN_FILE if minimal else OUTPUT_FILE
+    )
+    with open(output_path, 'wt') as output:
+        output.writelines('\n'.join(lines))
+
+
+if __name__ == '__main__':
+    main()
